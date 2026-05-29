b59f08993e6f9f6ff051efe931edc8994d8c738b
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-05-29 21:27:58 +0530

    Sql installation

--- mysql.sh
+++ mysql.sh
@@ -3,10 +3,27 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ]
 then
     echo "Error:: You should have root access to execute this script"
-    #exit 1
+    exit 1
 fi
 
-dnf install mysql-server -y 
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+
+dnf install mysql-server -y
+validate $? "Mysql Server installation"
